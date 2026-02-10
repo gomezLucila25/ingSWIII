19a9c78d6ec03bbcdea643ea08bd4d6f7efce41f
lucilaGomez <94055912+lucilaGomez@users.noreply.github.com>
2026-02-10 01:26:43 -0300

    Fix production environment: update API URL from Azure to GCP backend-prod

--- tf-frontend/src/environments/environment.prod.ts
+++ tf-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://userapi-lucila.azurewebsites.net/api/v1'
+  apiUrl: 'https://userapi-prod-1077879234681.us-central1.run.app/api/v1'
 };
